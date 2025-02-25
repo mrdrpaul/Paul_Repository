11f396f9edf3dcee0db8fc1edb181907273692db
Paul Cruz <paul.cruz10.mil@army.mil>
2025-02-25 10:12:04 -0600

    adding javascript file script.js

---
+++ JAVASCRIPT/JSDemo1/script.js
@@ -0,0 +1,9 @@
+// console.log("What is your age?")
+// variables
+
+data readline = read("Welcome to the game")
+cat = "tabby"
+cat2 = "orange"
+cat3 = 12
+console.log(cat)
+console.log(cat2 , cat + cat3)
